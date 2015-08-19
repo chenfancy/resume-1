dbd3b983c1d1501ee92228393c2682c76a758e02
flfwzgl <flfwzgl@qq.com>
2015-08-20 01:30:23 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -3,44 +3,46 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var minifyCss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
+var jade = require('gulp-jade');
+var rename = require('gulp-rename');
 
-var rtt = require('rtt');
 
 //compile less
 gulp.task('less', function(){
-	gulp.src('mobile/css/*.less')
+	gulp.src(['**/*.less', '!node_modules/**/*.*'])
 		.pipe(less())
 		.pipe(minifyCss())
-		.pipe(gulp.dest('../www/mobile/css/'));
-	gulp.src('css/*.less')
-		.pipe(less())
-		.pipe(minifyCss())
-		.pipe(gulp.dest('../www/css/'));
+		.pipe(gulp.dest('../www'));
 });
 
-//compress js
+//compress and rename *.js
 gulp.task('uglify', function(){
-	gulp.src('js/*.js')
+	gulp.src(['js/*.js', '!js/*.min.js'], {base: './'})
 		.pipe(uglify())
-		.pipe(gulp.dest('../www/js'));
-	gulp.src('mobile/js/*.js')
-		.pipe(uglify())
-		.pipe(gulp.dest('../www/mobile/js'));
+		.pipe(rename(function(path){
+			path.basename += '.min';
+		}))
+		.pipe(gulp.dest('../www/'));
 })
 
 //copy fonts file
 gulp.task('copy', function(){
 	gulp.src('css/fonts/*.*')
-		.pipe(gulp.dest('../www/css/fonts/'))
+		.pipe(gulp.dest('../www/css/fonts/', {overrite: true}))
 })
 
-//replace templete
-gulp.task('rtt', function(){
-	rtt('index.src.html', '../www/index.html', 'data/data.json');
-	rtt('mobile/index.src.html', '../www/mobile/index.html', 'data/data.json');
+//compile jade
+gulp.task('jade', function(){
+	gulp.src(['**/*.src.jade'])
+		.pipe(jade({pretty: false}))
+		.pipe(rename(function(path){
+			path.basename = path.basename.replace(/\.src$/, '');
+			path.extname = '.html'
+		}))
+		.pipe(gulp.dest('../www'))
 })
 
 
 gulp.task('default', function(){
-	gulp.run('less', 'uglify', 'rtt', 'copy');
+	gulp.run('less', 'uglify', 'jade', 'copy');
 });
