cc402e6865749933128754ce98b6ac5bbf7410f2
flfwzgl <flfwzgl@qq.com>
2016-02-20 03:03:20 +0800

    up

--- js/index.js
+++ js/index.js
@@ -155,6 +155,50 @@ window.onload = function(){
 	})();*/
 
 
+
+	var me_url = fn.browser.mac ? 'img/wo.mac.png' : 'img/wo.png';
+	if( fn.browser.support ){
+		fn.img([bgUrl], function(over){
+			if(over){
+				var bg = document.createElement('img');
+				bg.src = this.src;
+				bg.style.cssText = 'position:absolute; opacity:0;';
+				fn.id('stage').insertBefore(bg, fn.id('stage').firstChild);
+
+				setTimeout(function(){
+					bg.style.cssText = 'position:absolute; opacity:1; -webkit-transition-duration:1s; -moz-transition-duration:1s'
+				}, 100);
+			}
+		});
+
+		fn.img([config.base + me_url], function(over){
+			if(over){
+				var avatarContainer = fn.cls('avatar-container', 'div')[0];
+				var svg = avatarContainer.getElementsByTagName('svg')[0];
+				var path = svg.getElementsByTagName('path');
+				var avatar = document.createElement('img');
+				avatar.src = this.src;
+				avatar.style.opacity = 0;
+				avatarContainer.appendChild(avatar);
+
+				fn.move.ease([2100, 1200], 6000, function(v){
+					fn.each(path, function(i, e){
+						e.style.strokeDashoffset = v;	
+					});
+				}, function(){
+					fn.move.ease([1, 0], 1000, function(v){
+						svg.style.opacity = v;
+					}, function(){
+						svg.parentNode.removeChild(svg);
+					});
+					fn.move.ease([0, 1], 2000, function(v){
+						avatar.style.opacity = v;
+					})
+				})
+			}
+		});
+	}
+
 	(function(){
 		//3d作品展示
 		var w_ul = fn.cls('w_ul', 'ul')[0],
@@ -328,50 +372,7 @@ window.onload = function(){
 			return minIndex;
 		}
 	})();
-
-	var me_url = fn.browser.mac ? 'img/wo.mac.png' : 'img/wo.png';
-	if( fn.browser.support ){
-		fn.img([bgUrl], function(over){
-			if(over){
-				var bg = document.createElement('img');
-				bg.src = this.src;
-				bg.style.cssText = 'position:absolute; opacity:0;';
-				fn.id('stage').insertBefore(bg, fn.id('stage').firstChild);
-
-				setTimeout(function(){
-					bg.style.cssText = 'position:absolute; opacity:1; -webkit-transition-duration:1s; -moz-transition-duration:1s'
-				}, 100);
-			}
-		});
-
-		fn.img([config.base + me_url], function(over){
-			if(over){
-				var avatarContainer = fn.cls('avatar-container', 'div')[0];
-				var svg = avatarContainer.getElementsByTagName('svg')[0];
-				var path = svg.getElementsByTagName('path');
-				var avatar = document.createElement('img');
-				avatar.src = this.src;
-				avatar.style.opacity = 0;
-				avatarContainer.appendChild(avatar);
-
-				fn.move.ease([2100, 1200], 6000, function(v){
-					fn.each(path, function(i, e){
-						e.style.strokeDashoffset = v;	
-					});
-				}, function(){
-					fn.move.ease([1, 0], 1000, function(v){
-						svg.style.opacity = v;
-					}, function(){
-						svg.parentNode.removeChild(svg);
-					});
-					fn.move.ease([0, 1], 2000, function(v){
-						avatar.style.opacity = v;
-					})
-				})
-			}
-		});
-	}
-
+	
 	document.body.ondragstart = function(){ return false; }
 
 }
