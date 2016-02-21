810ceb619afc35a131f36c72c8529b3566945a4c
flfwzgl <flfwzgl@qq.com>
2016-02-22 00:14:49 +0800

    up

--- entry.js
+++ entry.js
@@ -34,8 +34,8 @@
 							move.ease([1, 0], 800, v => svg.style.opacity = v, () => {
 								svg.parentNode.removeChild(svg)
 								// move.collision([2, 1], 1500, v => {
-								// 	contactBox.style.transform = `scaleY(${v})`;
-								// 	contactBox.style.webkitTransform = `scaleY(${v})`;
+								//  contactBox.style.transform = `scaleY(${v})`;
+								//  contactBox.style.webkitTransform = `scaleY(${v})`;
 								// })
 								move.collision([90, 0], 1500, v => {
 									contactBox.style.transform = `rotateY(${v}deg)`;
@@ -91,9 +91,9 @@
 		let n = face.length, i=0;
 
 		
-		cube.unit = -360/n,								//每次旋转变化的角度
-		cube.cur = cube.old = Math.max( 0, page.indexOf(param) ),	//设置当前页,如果没有参数则默认首页
-		cube.a = cube.unit*cube.cur,					//要旋转到的角度
+		cube.unit = -360/n,               //每次旋转变化的角度
+		cube.cur = cube.old = Math.max( 0, page.indexOf(param) ), //设置当前页,如果没有参数则默认首页
+		cube.a = cube.unit*cube.cur,          //要旋转到的角度
 		cube.time = 0.3*n;
 
 
@@ -130,9 +130,9 @@
 			})
 			//点击前进后退触发事件处理程序
 			// .bind(window, 'popstate', function(e){
-			// 	var state = e.state;
-			// 	changePage(cube.cur=state.cur);
-			// 	e = null;
+			//  var state = e.state;
+			//  changePage(cube.cur=state.cur);
+			//  e = null;
 			// })
 			.bind(window, 'hashchange', function() {
 				var n = page.indexOf(location.hash.replace(/^#!/g, ''));
@@ -166,7 +166,7 @@
 				else if(e.keyCode === 39 || e.keyCode === 40) delta = 3;
 			} 
 
-			if(cube.running === true) return;	//防止滑轮滚动的时候乱窜
+			if(cube.running === true) return; //防止滑轮滚动的时候乱窜
 			cube.running = true;
 			setTimeout(() => cube.running = false, cube.time*1000);
 
@@ -192,7 +192,7 @@
 			cube.style.webkitTransform = `translate3d(0, 0, ${-cube.d}px) rotateY(${cube.a}deg)`;
 			cube.style.transform = `translate3d(0, 0, ${-cube.d}px) rotateY(${cube.a}deg)`;
 
-			for(i=0; i<n; i++) navLi[i].className = i===page ? 'active' : '';	
+			for(i=0; i<n; i++) navLi[i].className = i===page ? 'active' : ''; 
 		}
 
 	}
@@ -233,11 +233,11 @@
 				if(o.tagName === 'IMG'){
 					//将n得到, n代表第几个加载完成,从0开始, i代表在url中位于第几个
 					let num = +o.dataset.serial;
-					if(!(num < +Infinity)) return;	//防止num为NaN出错
+					if(!(num < +Infinity)) return;  //防止num为NaN出错
 
 					show.style.backgroundImage = 'url(../img/close.png)';
 					show.style.display = 'block';
-					show.scrollTop = fn.pos(itag[num]).top - 30;	
+					show.scrollTop = fn.pos(itag[num]).top - 30;  
 				}
 			})
 			.bind(show, 'mousemove', function(e){
@@ -248,8 +248,8 @@
 
 					let imgs = fn.tag('*', o.parentNode);
 					// if(imgs.length < loadAllUrl[serial].length) return;
-				  // let n = Math.floor( (e.clientX - fn.pos(o).left)*loadAllUrl[serial].length/o.offsetWidth );
-				  let n = Math.floor( (e.clientX - fn.pos(o).left)*imgs.length/o.offsetWidth );
+					// let n = Math.floor( (e.clientX - fn.pos(o).left)*loadAllUrl[serial].length/o.offsetWidth );
+					let n = Math.floor( (e.clientX - fn.pos(o).left)*imgs.length/o.offsetWidth );
 					n = Math.max(0, Math.min(loadAllUrl[serial].length - 1, n));
 					if(o.parentNode.n !== n) {
 						//切换图片
@@ -267,7 +267,7 @@
 
 		//先加载外面显示的图片,,等全部加载完成之后才加载其他图片
 		fn.img(coverUrl, function(over){
-			loadAllUrl.push(allUrl[this.index]);	//将allUrl按cover的加载顺序重排
+			loadAllUrl.push(allUrl[this.index]);  //将allUrl按cover的加载顺序重排
 			//如果是前几张就按照顺序排列, 第二行就按照哪个短排列哪个
 			w_li[shortIndex(w_li)].innerHTML += 
 				`<i>
@@ -303,7 +303,7 @@
 									I.insertBefore(img, imgs[i]);
 									break;
 								}
-							}						
+							}           
 						}
 					}
 				});
@@ -361,7 +361,7 @@
 		}
 	};
 
-	document.body.ondragstart = function(){ return false; }
+	document.body.ondragstart = () => false;
 
 })();
 
